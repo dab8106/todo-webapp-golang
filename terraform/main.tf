9903e2c4511ac186dc99acd58b6ec94bbd9cd922
Dabeer Shaikh <dab8106@gmail.com>
2023-10-29 00:16:34 +0530

    added terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tf29102023"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
